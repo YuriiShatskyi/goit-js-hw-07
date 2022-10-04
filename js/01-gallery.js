57861b4f6f11bd78651d5a7ce545b794adbe20d8
Yurii Shatskyi <yurii.shatskyi.it@gmail.com>
2022-10-04 16:32:43 +0300

    html-added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divPhotos = document.querySelector('.gallery');
+
+function createPhotoGallery(items) {
+    return items.map(item =>
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join('');
+};
+
+const addPhotoGallery = createPhotoGallery(galleryItems);
+
+divPhotos.insertAdjacentHTML("afterbegin", addPhotoGallery);
+
+divPhotos.addEventListener('click', onPhotoClick); 
+
+function onPhotoClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+
+
+    console.log('ТОЛІК');
+}
